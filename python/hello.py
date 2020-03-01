bf932f83fb6aed921f802ca4c37fd6f5b326e2b2
tafe <tafe@600900J127NS501.NSR.EDU.WIN>
2020-03-01 21:36:38 +1100

    commit

---
+++ python/hello.py
@@ -0,0 +1,16 @@
+def round_to_two_places(num):
+    """Return the given number rounded to two decimal places. 
+    
+    >>> round_to_two_places(3.14159)
+    3.14
+    """
+    # Replace this body with your own code.
+    # ("pass" is a keyword that does literally nothing. We used it as a placeholder
+    # because after we begin a code block, Python requires at least one line of code)
+    c = round(num,2)
+    return c
+
+print(round_to_two_places(3.112222))
+
+help(round)
+
